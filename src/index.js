428723330126ee776f947950cb4ddc4a114e7f48
Dmitry <petrov6827@gmail.com>
2020-04-13 12:38:36 +0300

    создан store, используя ООП, сделан рефаторинг кода

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -12,17 +12,17 @@ let rerenderEntireTree = (state) => {
     <Router>
       <App
         state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
       />
     </Router>,
     document.getElementById('root')
   );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,55 +1,60 @@
-let rerenderEntireTree = () => {
-  console.log('state changed');
-};
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, how r u? ', likesCount: '20' },
+        { id: 2, message: "It's my second post", likesCount: '5' },
+        { id: 3, message: 'What sports do u like more? ', likesCount: '2' },
+      ],
+      newPostText: '123',
+    },
 
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hi, how r u? ', likesCount: '20' },
-      { id: 2, message: "It's my second post", likesCount: '5' },
-      { id: 3, message: 'What sports do u like more? ', likesCount: '2' },
-    ],
-    newPostText: '123',
+    dialogsPage: {
+      messages: [
+        { id: 1, message: 'Привет пошли на футбол' },
+        { id: 2, message: 'Ты где был?' },
+        { id: 3, message: 'Занятия отменяются' },
+        { id: 4, message: 'Скидываемся по 200 руб' },
+      ],
+      dialogs: [
+        { id: 1, name: 'Алексей Бокс' },
+        { id: 2, name: 'Антон Футбол' },
+        { id: 3, name: 'Наташка Плавание' },
+        { id: 4, name: 'Миша Парашют' },
+        { id: 5, name: 'Андрей ' },
+        { id: 6, name: 'Борис борцуха' },
+      ],
+    },
   },
 
-  dialogsPage: {
-    messages: [
-      { id: 1, message: 'Привет пошли на футбол' },
-      { id: 2, message: 'Ты где был?' },
-      { id: 3, message: 'Занятия отменяются' },
-      { id: 4, message: 'Скидываемся по 200 руб' },
-    ],
-    dialogs: [
-      { id: 1, name: 'Алексей Бокс' },
-      { id: 2, name: 'Антон Футбол' },
-      { id: 3, name: 'Наташка Плавание' },
-      { id: 4, name: 'Миша Парашют' },
-      { id: 5, name: 'Андрей ' },
-      { id: 6, name: 'Борис борцуха' },
-    ],
+  getState() {
+    return this._state;
+  },
+
+  _callSubscriber() {
+    console.log('state changed');
+  },
+
+  addPost() {
+    let newPost = {
+      id: 4,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-window.state = state;
-
-export const addPost = () => {
-  let newPost = {
-    id: 4,
-    message: state.profilePage.newPostText,
-    likesCount: 14,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree();
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
+window.store = store;
