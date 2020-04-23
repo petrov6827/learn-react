03c7f615f95f1baf57e4cf380baab18976657da4
Dmitry <petrov6827@gmail.com>
2020-04-23 23:57:27 +0300

    постраничный вывод пользователей

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,89 +3,67 @@ import s from './Users.module.scss';
 import * as axios from 'axios';
 import userPhoto from '../../../src/assets/images/user.png';
 
-const Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios
-        .get('https://social-network.samuraijs.com/api/1.0/users')
-        .then((response) => {
-          props.setUsers(response.data.items);
-        });
-    }
-  };
-  // props.setUsers(
-  //   (users: [
-  //     {
-  //       id: 1,
-  //       photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
-  //       followed: false,
-  //       fullname: 'Dmitry',
-  //       status: 'Я босс',
-  //       location: { city: 'Moscow', country: 'Belarus' },
-  //     },
-  //     {
-  //       id: 2,
-  //       photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
-  //       followed: true,
-  //       fullname: 'Sasha',
-  //       status: 'Я тоже босс',
-  //       location: { city: 'Minsk', country: 'Russia' },
-  //     },
-  //     {
-  //       id: 3,
-  //       photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
-  //       followed: false,
-  //       fullname: 'Anton',
-  //       status: 'Я не босс',
-  //       location: { city: 'Kiev', country: 'Ukraine' },
-  //     }
-  //   ])
-  // );
+class Users extends React.Component {
+  componentDidMount() {
+    axios
+      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .then((response) => {
+        this.props.setUsers(response.data.items); //добавили this.
+      });
+  }
 
-  return (
-    <div className={s.users}>
-      <h1> Пользователи </h1>
-      <button className={s.getUsers} onClick={getUsers}>
-        Покажи пользователей
-      </button>
-      <div>
-        {props.users.map((u) => (
-          <div key={u.id}>
-            <div>
-              <img
-                src={u.photos.small != null ? u.photos.small : userPhoto}
-                className={s.img}
-                alt="аватарка"
-              />
+  render() {
+    return (
+      <div className={s.users}>
+        <h1> Пользователи </h1>
+        <div className={s.pages}>
+          <span className={s.pages__item}>1</span>
+          <span className={(s.pages__item, s.selectedPage)}>2</span>
+          <span className={s.pages__item}>3</span>
+          <span className={s.pages__item}>4</span>
+          <span className={s.pages__item}>5</span>
+        </div>
+
+        <div>
+          {this.props.users.map((u) => (
+            <div className={s.user__item} key={u.id}>
+              <div>
+                <img
+                  src={u.photos.small != null ? u.photos.small : userPhoto}
+                  className={s.img}
+                  alt="аватарка"
+                />
+                <div>
+                  {u.followed ? (
+                    <button
+                      onclick={() => {
+                        this.props.follow(u.id);
+                      }}
+                    >
+                      Отписаться
+                    </button>
+                  ) : (
+                    <button
+                      onclick={() => {
+                        this.props.unfollow(u.id);
+                      }}
+                    >
+                      Подписаться
+                    </button>
+                  )}
+                </div>
+              </div>
+
+              <div className={s.about}>{u.name}</div>
+              <div className={s.about}>{u.status}</div>
+              <div className={s.about}>{'u.location.country'}</div>
+              <div className={s.about}>{'u.location.city'}</div>
             </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onclick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Отписаться
-                </button>
-              ) : (
-                <button
-                  onclick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Подписка
-                </button>
-              )}
-            </div>
-            <div>{u.name}</div>
-            <div>{u.status}</div>
-            <div>{'u.location.country'}</div>
-            <div>{'u.location.city'}</div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,4 +1,4 @@
-import UsersС from './UsersС';
+import Users from './Users';
 import { connect } from 'react-redux';
 import { followAC, unfollowAC, setUsersAC } from '../../redux/users-reducer';
 
@@ -22,4 +22,4 @@ let mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersС);
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
