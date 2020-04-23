eb08a2ca8814e366b1ede8ed89d241d75b86e391
Dmitry <petrov6827@gmail.com>
2020-04-24 02:48:20 +0300

    отображение юзеров по клику на номер страницы с пользователями

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,22 +6,50 @@ import userPhoto from '../../../src/assets/images/user.png';
 class Users extends React.Component {
   componentDidMount() {
     axios
-      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
       .then((response) => {
-        this.props.setUsers(response.data.items); //добавили this.
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
       });
   }
 
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items); //добавили this.
+      });
+  };
+
   render() {
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+
     return (
       <div className={s.users}>
         <h1> Пользователи </h1>
         <div className={s.pages}>
-          <span className={s.pages__item}>1</span>
-          <span className={(s.pages__item, s.selectedPage)}>2</span>
-          <span className={s.pages__item}>3</span>
-          <span className={s.pages__item}>4</span>
-          <span className={s.pages__item}>5</span>
+          {pages.map((p) => (
+            <span
+              className={this.props.currentPage === p && s.selectedPage}
+              onClick={(e) => {
+                this.onPageChanged(p);
+              }}
+            >
+              {p}
+            </span>
+          ))}
         </div>
 
         <div>
